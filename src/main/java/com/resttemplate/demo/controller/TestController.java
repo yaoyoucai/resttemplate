2f981a23ae708172c15e9df034628cedf0fb8a89
huanyao <yaohuan@mapout.net>
2021-07-08 18:26:39 +0800

    为resttemplate增加text/html的支持

--- src/main/java/com/resttemplate/demo/controller/TestController.java
+++ src/main/java/com/resttemplate/demo/controller/TestController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author huanyao
  * @version 1.0
@@ -24,9 +27,35 @@ public class TestController {
     @Value("${url}")
     private String url;
 
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(TestController.class);
+
     @PostMapping("post")
     public void testPost() {
         String value = restTemplate.postForObject(url, new Object().toString(), String.class);
         System.out.println(value);
+    }
+
+    @PostMapping("testGetForEntity")
+    public void testGetForEntity() {
+        //请求方式1
+        String value = restTemplate.getForObject(url+"findPet?petId={1}",String.class,3434343);
+
+        //请求方式2
+        Map<String, Object> values = new HashMap<>();
+        values.put("petId", 45667787);
+        value = restTemplate.getForObject(url+"findPet?petId={petId}",String.class,values);
+        log.info("value={}",value);
+    }
+
+    @PostMapping("testGetForObject")
+    public void testGetForObject() {
+        //请求方式1
+        String value = restTemplate.getForObject(url+"findPet?petId={1}",String.class,3434343);
+
+        //请求方式2
+        Map<String, Object> values = new HashMap<>();
+        values.put("petId", 45667787);
+        value = restTemplate.getForObject(url+"findPet?petId={petId}",String.class,values);
+        log.info("value={}",value);
     }
 }
